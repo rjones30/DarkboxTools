47907384cb9eaa072bcc9f9012eaf8f591b61598
Richard Jones <rjones30@gmail.com>
2016-06-01 10:14:09 -0400

    * fiberQA_cron.sh [rtj]    - add shell commands to allow histograms to be written by the apache      user into a histos directory whose owner is another member of a group      of which apache is also a member, assumes the host dir has group w perms.    - add a line to the output log that lets runcond.py know where the output      from a given run ends before the point where runcond.py is first run. * plotmaker.py [rtj]    - bug fixes

--- fiberQA_cron.sh
+++ fiberQA_cron.sh
@@ -54,7 +54,10 @@ if [[ -d $base/newrun-requested ]]; then
     ./treemaker $evtfile || exit 1
     run=`echo $evtfile | sed 's/^.*simple_\([0-9]*\).evt.0/\\1/'`
     mkdir histos/run_$run || exit 1
+    chgrp prod histos/run_$run || exit 1
+    chmod g+w histos/run_$run || exit 1
     export DISPLAY=:23
+    echo Successful collection of run $run
     ./plotmaker.py $run
     echo Successful collection and analysis of run $run
     rm -rf newrun-requested newrun-underway
--- plotmaker.py
+++ plotmaker.py
@@ -12,12 +12,28 @@
 gainPC = 0.45
 counts_per_pixel = 4096. / 400 * gainPC
 
-from ROOT import *
+import subprocess
 import sys
+import os
+import re
+import math
+
+from ROOT import *
 
 def usage():
    print "Usage: plotmaker.py <run_number>"
    sys.exit(1)
+
+def get_conditions(run):
+   conditions = {}
+   runcond = os.path.dirname(sys.argv[0]) + "/runcond.py"
+   response = subprocess.Popen([runcond, str(run)], stdout=subprocess.PIPE).communicate()[0]
+   for cond in response.split('\n'):
+      m1 = re.match(r" *([^ ]+) level = +([.0-9]+) .*", cond)
+      if m1:
+         conditions[m1.group(1)] = float(m1.group(2))
+         print "just loaded condition for var", m1.group(1), "as", m1.group(2)
+   return conditions
 
 def plot(run):
    rootfile = "data/simple_{0:03d}.root".format(run)
@@ -26,6 +42,23 @@ def plot(run):
       print "Input file", rootfile, "not found, cannot continue."
       sys.exit(1)
    c1 = TCanvas("c1","C1",20,20,400,400);
+
+   # These temperature correction factors were derived from a study by James McIntyre
+   # and reported to me [rtj] in an email dated May 19, 2016.
+
+   if "Tpreamp1" in conditions:
+      tcorr1 = math.exp(0.101 * (conditions["Tpreamp1"] - 25))
+   else:
+      tcorr1 = 1
+   if "Tpreamp2" in conditions:
+      tcorr2 = math.exp(0.121 * (conditions["Tpreamp2"] - 25))
+   else:
+      tcorr2 = 1
+   print "Temperature correction factors are", tcorr1, tcorr2
+
+   hdir = "histos/run_" + str(run)
+   for oldimg in os.listdir(hdir):
+      os.unlink(hdir + "/" + oldimg)
 
    hist = []
    spigot = [5, 6, 7, 8, 15, 10];
@@ -36,7 +69,8 @@ def plot(run):
       if v792.Draw(qvar, qnot) > 0:
          htemp = gROOT.FindObject("htemp")
          pedestal = htemp.GetMean()
-         qvar = "(" + qvar + "-" + str(pedestal) + ")/" + str(counts_per_pixel)
+         qvar = "(" + qvar + "-" + str(pedestal) + ")*" + \
+                                   str(tcorr1) + "/" + str(counts_per_pixel)
          v792.Draw(qvar, qcon)
          htemp = gROOT.FindObject("htemp")
          htemp.SetTitle("fiber {0:d}".format(chan + 1))
@@ -49,8 +83,8 @@ def plot(run):
       if v792.Draw(qvar2, qnot) > 0:
          htemp = gROOT.FindObject("htemp")
          pedestal2 = htemp.GetMean()
-         qvar2 = "(" + qvar2 + "-" + str(pedestal2) + ")/" + \
-                                     str(counts_per_pixel)
+         qvar2 = "(" + qvar2 + "-" + str(pedestal2) + ")*" + \
+                                     str(tcorr2) + "/" + str(counts_per_pixel)
          v792.Draw(qvar2, qcon)
          htemp = gROOT.FindObject("htemp")
          htemp.SetTitle("fiber {0:d}".format(chan + 16))
@@ -68,4 +102,6 @@ try:
    run = int(sys.argv[1])
 except ValueError:
    usage()
+
+conditions = get_conditions(run)
 plot(run)
